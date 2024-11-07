597b0b0c139dce751d87e8736fff0fcd5ff695fd
Thomas Loh <lohjian@graduate.utm.my>
2024-11-07 22:55:59 +0800

    simple calc python

--- test.py
+++ test.py
@@ -1,10 +1,43 @@
-a = 2
-b = 2
+from sklearn import svm
 
-if(a == 2):
-    print("True")
-else:
-    print("False")
+def addition(x, y):
+    total = x + y
+    return total
 
-if(a == b):
-    print("True")
+def subtraction(x, y):
+    total = x - y
+    return total
+
+def division(x, y):
+    total = x / y
+    return total
+
+def multiplication(x, y):
+    total = x * y
+    return total
+
+def main():
+    num1 = float(input("First Number:"))
+    num2 = float(input("Second Number:"))
+    operator = input("Operator:")
+    
+    if num1 < 0:
+        print("First number cannot be negative.")
+    elif num2 < 0:
+        print("Second number cannot be negative.")
+    elif operator == "+":
+        total = addition(num1, num2)
+        print("Total = ", total)
+    elif operator == "-":
+        total = subtraction(num1, num2)
+        print("Total = ", total)
+    elif operator == "*":
+        total = multiplication(num1, num2)
+        print("Total = ", total)
+    elif operator == "/":
+        total = division(num1, num2)
+        print("Total = ", total)
+    else:
+        print("Invalid operator")
+
+main()
